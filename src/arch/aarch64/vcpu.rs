355095aba29129825d8328f9be70fedb089470f6
hw0505 <39039541+hw0505@users.noreply.github.com>
2023-09-18 17:26:15 +0800

    add percup and vcpu

--- src/arch/aarch64/vcpu.rs
+++ src/arch/aarch64/vcpu.rs
@@ -311,3 +311,316 @@ pub fn vcpu_run(announce: bool) -> ! {
     }
 }
 */
+
+
+
+#[derive(Default)]
+/// A virtual CPU within a guest
+pub struct VCpu<H: HyperCraftHal> {
+    //vcpu_id: usize,
+    //regs: VmCpuRegisters,
+    // gpt: G,
+    // pub guest: Arc<Guest>,
+
+    pub id: usize,
+    pub phys_id: usize,
+    pub state: VcpuState,
+    pub vm: Option<Vm>,
+    pub int_list: Vec<usize>,
+    pub vcpu_ctx: ContextFrame,
+    pub vm_ctx: VmContext,
+
+    marker: PhantomData<H>,
+}
+
+impl<H: HyperCraftHal> VCpu<H> {
+
+    // /// Create a new vCPU
+    // pub fn new(vcpu_id: usize, entry: GuestPhysAddr) -> Self {
+    //     Self {
+    //         vcpu_id,
+    //         phys_id: 0,
+    //         state: VcpuState::VcpuInv,
+    //         vm: None,
+    //         int_list: vec![],
+    //         vcpu_ctx: ContextFrame::default(),
+    //         vm_ctx: VmContext::default(),
+    //         marker: PhantomData,
+    //     }
+    // }
+
+
+    /// Create a new vCPU
+    pub fn new(vcpu_id: usize, entry: GuestPhysAddr) -> Self {
+        // let mut regs = VmCpuRegisters::default();
+        // // Set hstatus
+        // let mut hstatus = LocalRegisterCopy::<usize, hstatus::Register>::new(
+        //     riscv::register::hstatus::read().bits(),
+        // );
+        // hstatus.modify(hstatus::spv::Supervisor);
+        // // Set SPVP bit in order to accessing VS-mode memory from HS-mode.
+        // hstatus.modify(hstatus::spvp::Supervisor);
+        // CSR.hstatus.write_value(hstatus.get());
+        // regs.guest_regs.hstatus = hstatus.get();
+
+        // // Set sstatus
+        // let mut sstatus = sstatus::read();
+        // sstatus.set_spp(sstatus::SPP::Supervisor);
+        // regs.guest_regs.sstatus = sstatus.bits();
+
+        // regs.guest_regs.gprs.set_reg(GprIndex::A0, 0);
+        // regs.guest_regs.gprs.set_reg(GprIndex::A1, 0x9000_0000);
+
+        // // Set entry
+        // regs.guest_regs.sepc = entry;
+
+        let mut vcpu_ctx = ContextFrame::default();
+        let mut vm_ctx = VmContext::default();
+        let arg = &config.memory_region()[0]; //need to fix
+
+
+        vcpu_ctx.set_argument(arg.ipa_start + arg.length); //need to fix
+        //vcpu_ctx.set_exception_pc(config.kernel_entry_point()); //need to fix
+        vcpu_ctx.set_exception_pc(entry);
+        vcpu_ctx.spsr =
+        (SPSR_EL1::M::EL1h + SPSR_EL1::I::Masked + SPSR_EL1::F::Masked + SPSR_EL1::A::Masked + SPSR_EL1::D::Masked)
+            .value;
+
+        //arch_ctx_reset
+        vm_ctx.cntvoff_el2 = 0;
+        vm_ctx.sctlr_el1 = 0x30C50830;
+        vm_ctx.cntkctl_el1 = 0;
+        vm_ctx.pmcr_el0 = 0;
+        vm_ctx.vtcr_el2 = 0x8001355c;
+        // }
+        let mut vmpidr = 0;
+        vmpidr |= 1 << 31;
+
+        // #[cfg(feature = "tx2")]
+        // if self.vm_id() == 0 {
+        //     // A57 is cluster #1 for L4T
+        //     vmpidr |= 0x100;
+        // }
+
+        vmpidr |= vcpu_id;
+        vm_ctx.vmpidr_el2 = vmpidr as u64;
+
+        //gic_ctx_reset
+        use crate::arch::gich_lrs_num;
+        for i in 0..gich_lrs_num() {
+            vm_ctx.gic_state.lr[i] = 0;
+        }
+        vm_ctx.gic_state.hcr |= 1 << 2;
+
+        
+        Self {
+            vcpu_id,
+            phys_id: 0,
+            state: VcpuState::VcpuInv,
+            vm: None,
+            int_list: vec![],
+            vcpu_ctx: vcpu_ctx,
+            vm_ctx: vm_ctx,
+            marker: PhantomData,
+        }
+    }
+
+    fn arch_ctx_reset(&mut self) {
+        // let migrate = self.vm.as_ref().unwrap().migration_state();
+        // if !migrate {
+        self.vm_ctx.cntvoff_el2 = 0;
+        self.vm_ctx.sctlr_el1 = 0x30C50830;
+        self.vm_ctx.cntkctl_el1 = 0;
+        self.vm_ctx.pmcr_el0 = 0;
+        self.vm_ctx.vtcr_el2 = 0x8001355c;
+        // }
+        let mut vmpidr = 0;
+        vmpidr |= 1 << 31;
+
+        #[cfg(feature = "tx2")]
+        if self.vm_id() == 0 {
+            // A57 is cluster #1 for L4T
+            vmpidr |= 0x100;
+        }
+
+        vmpidr |= self.id;
+        self.vm_ctx.vmpidr_el2 = vmpidr as u64;
+    }
+
+    fn reset_context(&mut self) {
+        // let migrate = self.vm.as_ref().unwrap().migration_state();
+        self.arch_ctx_reset();
+        // if !migrate {
+        self.gic_ctx_reset();
+        // }
+        use crate::kernel::vm_if_get_type;
+        match vm_if_get_type(self.vm_id()) {
+            VmType::VmTBma => {
+                println!("vm {} bma ctx restore", self.vm_id());
+                self.reset_vm_ctx();
+                self.context_ext_regs_store();
+            }
+            _ => {}
+        }
+    }
+
+    fn gic_ctx_reset(&mut self) {
+        use crate::arch::gich_lrs_num;
+        for i in 0..gich_lrs_num() {
+            self.vm_ctx.gic_state.lr[i] = 0;
+        }
+        self.vm_ctx.gic_state.hcr |= 1 << 2;
+    }
+
+    fn context_ext_regs_store(&mut self) {
+        self.vm_ctx.ext_regs_store();
+    }
+
+    fn reset_vm_ctx(&mut self) {
+        self.vm_ctx.reset();
+    }
+
+    fn set_elr(&mut self, elr: usize) {
+        self.vcpu_ctx.set_exception_pc(elr);
+    }
+
+    fn set_gpr(&mut self, idx: usize, val: usize) {
+        self.vcpu_ctx.set_gpr(idx, val);
+    }
+
+    fn show_ctx(&self) {
+        println!(
+            "cntvoff_el2 {:x}, sctlr_el1 {:x}, cntkctl_el1 {:x}, pmcr_el0 {:x}, vtcr_el2 {:x} x0 {:x}",
+            self.vm_ctx.cntvoff_el2,
+            self.vm_ctx.sctlr_el1,
+            self.vm_ctx.cntkctl_el1,
+            self.vm_ctx.pmcr_el0,
+            self.vm_ctx.vtcr_el2,
+            self.vcpu_ctx.gpr(0)
+        );
+    }
+
+
+
+    /// Initialize nested mmu.
+    pub fn init_page_map(&mut self, token: usize) {
+        // Set hgatp
+        // TODO: Sv39 currently, but should be configurable
+        self.regs.virtual_hs_csrs.hgatp = token;
+        unsafe {
+            core::arch::asm!(
+                "csrw hgatp, {hgatp}",
+                hgatp = in(reg) self.regs.virtual_hs_csrs.hgatp,
+            );
+            core::arch::riscv64::hfence_gvma_all();
+        }
+    }
+
+    /// Restore vCPU registers from the guest's GPRs
+    pub fn restore_gprs(&mut self, gprs: &GeneralPurposeRegisters) {
+        for index in 0..32 {
+            self.regs.guest_regs.gprs.set_reg(
+                GprIndex::from_raw(index).unwrap(),
+                gprs.reg(GprIndex::from_raw(index).unwrap()),
+            )
+        }
+    }
+
+    /// Save vCPU registers to the guest's GPRs
+    pub fn save_gprs(&self, gprs: &mut GeneralPurposeRegisters) {
+        for index in 0..32 {
+            gprs.set_reg(
+                GprIndex::from_raw(index).unwrap(),
+                self.regs
+                    .guest_regs
+                    .gprs
+                    .reg(GprIndex::from_raw(index).unwrap()),
+            );
+        }
+    }
+
+    /// Runs this vCPU until traps.
+    pub fn run(&mut self) -> VmExitInfo {
+        let regs = &mut self.regs;
+        unsafe {
+            // Safe to run the guest as it only touches memory assigned to it by being owned
+            // by its page table
+            _run_guest(regs);
+        }
+        // Save off the trap information
+        regs.trap_csrs.scause = scause::read().bits();
+        regs.trap_csrs.stval = stval::read();
+        regs.trap_csrs.htval = htval::read();
+        regs.trap_csrs.htinst = htinst::read();
+
+        let scause = scause::read();
+        use scause::{Exception, Interrupt, Trap};
+        match scause.cause() {
+            Trap::Exception(Exception::VirtualSupervisorEnvCall) => {
+                let sbi_msg = SbiMessage::from_regs(regs.guest_regs.gprs.a_regs()).ok();
+                VmExitInfo::Ecall(sbi_msg)
+            }
+            Trap::Interrupt(Interrupt::SupervisorTimer) => VmExitInfo::TimerInterruptEmulation,
+            Trap::Interrupt(Interrupt::SupervisorExternal) => {
+                VmExitInfo::ExternalInterruptEmulation
+            }
+            Trap::Exception(Exception::LoadGuestPageFault)
+            | Trap::Exception(Exception::StoreGuestPageFault) => {
+                let fault_addr = regs.trap_csrs.htval << 2 | regs.trap_csrs.stval & 0x3;
+                // debug!(
+                //     "fault_addr: {:#x}, htval: {:#x}, stval: {:#x}, sepc: {:#x}, scause: {:?}",
+                //     fault_addr,
+                //     regs.trap_csrs.htval,
+                //     regs.trap_csrs.stval,
+                //     regs.guest_regs.sepc,
+                //     scause.cause()
+                // );
+                VmExitInfo::PageFault {
+                    fault_addr,
+                    // Note that this address is not necessarily guest virtual as the guest may or
+                    // may not have 1st-stage translation enabled in VSATP. We still use GuestVirtAddr
+                    // here though to distinguish it from addresses (e.g. in HTVAL, or passed via a
+                    // TEECALL) which are exclusively guest-physical. Furthermore we only access guest
+                    // instructions via the HLVX instruction, which will take the VSATP translation
+                    // mode into account.
+                    falut_pc: regs.guest_regs.sepc,
+                    inst: regs.trap_csrs.htinst as u32,
+                    priv_level: PrivilegeLevel::from_hstatus(regs.guest_regs.hstatus),
+                }
+            }
+            _ => {
+                panic!(
+                    "Unhandled trap: {:?}, sepc: {:#x}, stval: {:#x}",
+                    scause.cause(),
+                    regs.guest_regs.sepc,
+                    regs.trap_csrs.stval
+                );
+            }
+        }
+    }
+
+    /// Gets one of the vCPU's general purpose registers.
+    pub fn get_gpr(&self, index: GprIndex) -> usize {
+        self.regs.guest_regs.gprs.reg(index)
+    }
+
+    /// Set one of the vCPU's general purpose register.
+    pub fn set_gpr(&mut self, index: GprIndex, val: usize) {
+        self.regs.guest_regs.gprs.set_reg(index, val);
+    }
+
+    /// Advance guest pc by `instr_len` bytes
+    pub fn advance_pc(&mut self, instr_len: usize) {
+        self.regs.guest_regs.sepc += instr_len
+    }
+
+    /// Gets the vCPU's id.
+    pub fn vcpu_id(&self) -> usize {
+        self.vcpu_id
+    }
+
+    /// Gets the vCPU's registers.
+    pub fn regs(&mut self) -> &mut VmCpuRegisters {
+        &mut self.regs
+    }
+}
